28956e6b0003c4e2cd5003ba6f4af9e9be3a063d
nathaniel kennis <drummer_39@att.net>
2020-10-22 16:10:42 -0500

    Testing

---
+++ Encryption/FileIO.h
@@ -0,0 +1,55 @@
+/// FileIO.h: file reading and writing header
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream> 
+using namespace std;
+
+class FileIO
+{
+public:
+    // Reads a supplied filename contents and returns a string array
+    static string fileRead(string filename)
+    {
+        ifstream file;
+        file.open(filename);
+
+        if (!file.is_open())
+        {
+            cout << "Error opening " << filename << endl;
+            return nullptr;
+        }
+
+        stringstream strStream;
+        strStream << file.rdbuf(); //read the file
+        
+        string file_text = strStream.str(); //str holds the content of the file
+
+        file.close();
+    	
+        return file_text;
+    }
+
+    // Writes a file with a supplied string value as append or overwrite
+    static bool fileWrite(string filename, string* file_text, bool isAppend)
+    {
+        ofstream file;
+        if (isAppend)
+            file.open(filename, ios_base::app);
+        else
+            file.open(filename);
+
+        if (!file.is_open())
+        {
+            cout << "Error opening " << filename << endl;
+            return false;
+        }
+
+        file << file_text->data() << endl;
+        file.close();
+        return true;
+    }
+};
+
+
--- Encryption/main.cpp
+++ Encryption/main.cpp
@@ -1,47 +1,91 @@
 #include <iostream>
+#include "FileIO.h"
+
 using namespace std;
 
+// Performs a XOR based encryption/decryption on supplied text from a given password 
 class Algorithm
 {
 	private:
 	
 	public:
-	static string Calculate(const char* text, const char* pass)
+	static string* Calculate(const char* text, const char* pass)
 	{
-		char ch;
-		int pass_idx = 0;
 		const int pass_length = strlen(pass);
 		const int text_length = strlen(text);
-		unsigned int crypt;
-
-		// cout << "Text:" << text << " Length:" << text_length << endl;
-		// cout << "Pass:" << pass << " Length:" << pass_length << endl;
+		int crypt;
+		int pass_idx = 0;
 		
-		string s;
+		string* s = new string();
 		char c;
 
-		for (int text_idx=0; text_idx < text_length;text_idx++)
+		// Iterate text characters and apply a rotating XOR per password value 
+		for (int text_idx=0; text_idx < text_length - 1;text_idx++)
 		{
 			crypt = (int)pass[pass_idx];
-			s.push_back(text[text_idx] ^ crypt);
+			
+			s->push_back(text[text_idx] ^ crypt);
 			
 			pass_idx++;
-			if (pass_idx >= pass_length)
+			if (pass_idx >= pass_length - 1)
 				pass_idx = 0;
 		}
 
-		return s.data();
+		return s;
 	}
 };
 
 
-int main()
+int main(int argc, char* argv[])
 {
-	const string encrypted = Algorithm::Calculate("Hello World! Yes it is working!", "password");
-	cout << "Encrypted:";
-	cout << string(encrypted) << endl;
-	const string decrypted = Algorithm::Calculate(encrypted.c_str(), "password");
-	cout << "Decrypted:";
-	cout << string(decrypted) << endl;
+	const int PASS_LENGTH_MIN = 8;
+	const int PASS_LENGTH_MAX = 20;
+	
+	if (argc < 4)
+	{
+		cerr << "Usage: " << endl <<
+			argv[0] << "'text file in' 'text file out' password" << endl
+			<< "Example: myfile1.txt myfile2.txt p@ssword1" << endl
+			<< "Example: myfile2.txt myfile3.txt p@ssword1" << endl;
+		return -1;
+	}
+	
+	if (strlen(argv[3]) < PASS_LENGTH_MIN || strlen(argv[3]) > PASS_LENGTH_MAX)
+	{
+		cout << "Invalid password length (" << strlen(argv[3]) << "). Must be between "
+		<< PASS_LENGTH_MIN << " and " << PASS_LENGTH_MAX << " characters." << endl;
+		return -1;
+	}
+
+	if (strlen(argv[1]) == 0 || strlen(argv[2]) == 0)
+	{
+		cout << "Invalid file names." << endl;
+		return -1;
+	}
+
+	cout << "Reading " << argv[1] << endl;
+
+	const string file_text_in = FileIO::fileRead(argv[1]);
+	
+	if (file_text_in.empty())
+	{
+		cout << "File was empty!" << endl;
+		return -1;
+	}
+
+	cout << "Processing Text ..." << endl;
+
+	string* file_text_out = Algorithm::Calculate(file_text_in.data(), "p1ssword2");
+
+	cout << "Writing to file " << argv[2] << endl;
+	
+	if (!FileIO::fileWrite(argv[2], file_text_out, false))
+	{
+		cout << "Operation Failed!" << endl;
+		return -1;
+	}
+	
+	cout << "Operation Completed." << endl;
+	return 0;
 }
 
